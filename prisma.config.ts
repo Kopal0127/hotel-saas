bc940b2041e113ee1208695a0996df6cd3b2b8b8
Tushar Dargude <dargudetushar@gmail.com>
2026-03-20 12:07:46 +0530

    Railway database config

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'prisma/config'
 
 export default defineConfig({
   datasource: {
-    url: "postgresql://postgres:Mottu%40123@localhost:5432/hoteldb?schema=public",
+    url: process.env.DATABASE_URL,
   }
 })
